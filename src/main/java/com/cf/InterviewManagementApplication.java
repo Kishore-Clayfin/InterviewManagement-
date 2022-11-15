e64ff4164d4ef64b777e995855628e191e237cb9
LUCKY <LUCKY@DESKTOP-IBDP8JQ>
2022-11-15 15:06:50 +0530

    Merge branch 'main' of https://github.com/Kishore-Clayfin/InterviewManagement-.git into main

--- src/main/java/com/cf/InterviewManagementApplication.java
+++ src/main/java/com/cf/InterviewManagementApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InterviewManagementApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hii");
 		SpringApplication.run(InterviewManagementApplication.class, args);
 	}
 
